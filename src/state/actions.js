7f175c1820dee3a78bcf5a2a5503089c43de0d66
Dmitry Mardukhayev <dmitrymar@yahoo.com>
2017-03-24 14:58:02 -0400

    Merged array of arrays and arranged by year and then title

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,20 +1,22 @@
-import moment from 'moment'
 import movies from './movies'
 
 
 export function getPopularMovies () {
-  //
-  // movies contains the results of two API requests
-  //
+  // Combine an array of arrays into a single array.
+  const combinedResults = [...movies[0], ...movies[1]];
+  combinedResults.forEach((el) => {
+    const { title } = el;
+    // Extract year that is at the end of title inside parenthesis and add it to new releaseYear attribute.
+    // Note: I was thinking of extracting year value from releaseDate but it doesn't seem to make sense
+    // since let's say for Forrest Gump it would display 1945 but the movie was released in 1994, that's
+    // why I decided to extract from title.
+    el.releaseYear = parseInt(title.substring((title.length - 5), (title.length - 1)));
+  });
 
-  //
-  // 1. combine the results of these requests
-  // 2. sort the results FIRST by year THEN by title
-  // 3. each movie object in the results needs a releaseYear attribute added
-  //    this is used in src/components/movies-list.js line 25
-  //
-
-  const combinedResults = []
+  // Sort by year then by title.
+  // First compare years. If the years are not equal then sort by year and ignore comparing titles
+  // If the years are equal then compare titles to see if one title comes before another alphabetically
+  combinedResults.sort((a, b) => a.releaseYear - b.releaseYear  ||  a.title.localeCompare(b.title));
 
   return {
     type: 'GET_MOVIES_SUCCESS',
